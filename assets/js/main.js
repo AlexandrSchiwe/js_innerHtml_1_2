0d905882dd58d39cd96c7b1420b18ba7862fb395
githubsupercode <alex.supercode@gmail.com>
2022-04-14 13:32:35 +0200

    lastpush

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// innerHTML 1.2
+
+document.getElementById('container').innerHTML;
+container.innerHTML = "<p>start of the element</p>";
+
+document.getElementById('info').innerHTML;
+info.innerHTML = "<h1>Hello Wolrd</h1>";
+
+document.write("<h2>How are you?</h2>");
+
+document.write("<p>end of the element</p>");
